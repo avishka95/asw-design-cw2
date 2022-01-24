fd093ebeedf7b284acc616ce279931de0517e71f
Avishka Ariyaratne <avishka95@gmail.com>
2022-01-24 12:33:40 +0530

    Endpoint change

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,4 +1,4 @@
-const API_ENDPOINT = "http://localhost:8080";
+const API_ENDPOINT = "http://localhost:8080/api/categories";
 export const APP_CONFIG = {
     APP_NAME: "Expense Tracker",
     APIS: {
